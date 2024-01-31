479a0610f9d1327be38a1ea1985a72f36ea3fd2a
NelsonPz <nelsonp.monsanto@gmail.com>
2024-01-30 23:01:47 -0500

    First commit

---
+++ src/main/java/comp74/week2/controllers/MainController.java
@@ -0,0 +1,172 @@
+package comp74.week2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import comp74.week2.model.Model;
+import comp74.week2.model.Posting;
+import comp74.week2.model.Profile;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    Model model;
+
+    public MainController(Model model) {
+        this.model = model;
+    }
+
+    @GetMapping("/profiles")
+    public List<Profile> getProfileByUserName(
+            @RequestParam(name = "userName", required = false, defaultValue = "") String userName) {
+        System.out.println("Username: " + userName);
+        if (userName.isEmpty())
+            return model.getProfiles();
+        else
+            return model.getProfilesByUserName(userName);
+    }
+
+    @GetMapping("/profiles/{profileId}")
+    public Profile getProfileById(@PathVariable Integer profileId) {
+        return model.getProfileById(profileId);
+    }
+
+    @GetMapping("/profiles/{profileId}/postings")
+    public List<Posting> getProfilePostings(@PathVariable Integer profileId) {
+        List<Posting> postings = null;
+        Profile profile = model.getProfileById(profileId);
+        if (profile != null)
+            postings = profile.getPostings();
+        return postings;
+    }
+
+    @PostMapping("/profiles")
+    public ResponseEntity<Profile> postMethodName(@RequestBody Profile profile) {
+        Profile newProfile = new Profile(profile.getUserName());
+        
+        ResponseEntity<Profile> profileEntity = model.addProfile(newProfile);
+       
+        return profileEntity;
+    }
+
+    @PostMapping("/profiles/{profileId}/postings")
+    public ResponseEntity<Posting> addPostingToProfile(@PathVariable Integer profileId, @RequestBody Posting posting) {
+        Profile profile = model.getProfileById(profileId);
+        if (profile != null) {
+            ResponseEntity<Posting> newPosting = model.addPosting(profile, posting);
+            return newPosting;
+        } else {
+            return null;
+        }
+    }
+
+    @PostMapping("/profiles/postings")
+    public ResponseEntity<Posting> addPostingByUsername(@RequestParam String userName, @RequestBody Posting posting) {
+        Profile profile = model.getProfilesByUserName(userName).get(0);
+        if (profile != null) {
+            ResponseEntity<Posting> newPosting = model.addPosting(profile, posting);
+            return newPosting;
+        } else {
+            return null;
+        }
+    }
+    @DeleteMapping("/profiles/{profileId}/postings/{postingId}")
+    public ResponseEntity<String> delelePostingById(@PathVariable Integer profileId, @PathVariable Integer postingId)
+    {
+        Profile profile = model.getProfileById(profileId);
+       
+        if(profile != null)
+        {
+            System.out.println("here1");
+            boolean postdeleted = model.deletePosting(profile, postingId);
+             
+            if(postdeleted)
+            {
+                return new ResponseEntity<>("Success",HttpStatus.NO_CONTENT);
+            } else
+            {
+                return new ResponseEntity<>("Either postingId or profileId does not exist.",HttpStatus.NOT_FOUND);
+   
+            }
+        } else {
+            return new ResponseEntity<>("Profile does not exist",HttpStatus.NOT_FOUND);
+        }
+        
+    }
+    @DeleteMapping("/profiles/{profileId}/postings")
+    public ResponseEntity<String> deleteAllPostingByProfile(@PathVariable Integer profileId)
+    {
+        Profile profile = model.getProfileById(profileId);
+        if(profile != null)
+        {
+            boolean postdeleted = model.deleteAllPostings(profile);
+            if(postdeleted)
+            {
+                return new ResponseEntity<>("Success",HttpStatus.NO_CONTENT);
+            } else
+            {
+                return new ResponseEntity<>("Either postingId or profileId does not exist.",HttpStatus.NOT_FOUND);
+   
+            }
+        } else {
+            return new ResponseEntity<>("Profile does not exist",HttpStatus.NOT_FOUND);
+        }    
+    }
+
+    @DeleteMapping("/profiles/{profileId}")
+    public ResponseEntity<String> deleleProfile(@PathVariable Integer profileId)
+    {
+        Profile profile = model.getProfileById(profileId);
+       
+        if(profile != null)
+        {
+            boolean postdeleted = model.deleteProfileById(profile);   
+            if(postdeleted)
+            {
+                return new ResponseEntity<>("Success",HttpStatus.NO_CONTENT);
+            } else
+            {
+                return new ResponseEntity<>("Profile does not exist",HttpStatus.NOT_FOUND);       
+            }
+        } else {
+            return new ResponseEntity<>("profleId exists but still has postings",HttpStatus.CONFLICT);
+        }
+    } 
+
+    @PutMapping("/profiles/{profileId}/postings/{postingId}")
+    public ResponseEntity<String> putMethodName(@PathVariable Integer profileId, @PathVariable Integer postingId, @RequestBody Posting posting) {
+        
+        Profile profile = model.getProfileById(profileId);
+       
+        if(profile != null)
+        {
+            boolean postdeleted = model.updatePosting(profile,posting,postingId);   
+            if(postdeleted)
+            {
+                return new ResponseEntity<>("Success",HttpStatus.NO_CONTENT);
+            } else
+            {
+                return new ResponseEntity<>("Profile does not exist",HttpStatus.NOT_FOUND);       
+            }
+        } else {
+            return new ResponseEntity<>("profleId exists but still has postings",HttpStatus.CONFLICT);
+        }
+    }
+
+}
+
+    
---
+++ src/main/java/comp74/week2/model/Model.java
@@ -0,0 +1,153 @@
+package comp74.week2.model;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.springframework.boot.context.config.Profiles;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Model {
+
+    HashMap<Integer, Profile> profiles;
+    HashMap<Integer, Posting> postings;
+    HashMap<String, Profile> userMap;
+
+    private static Integer nextProfileId = 100;
+    private static Integer nextPostingId = 100;
+
+    public Model() {
+        super();
+        profiles = new HashMap<>();
+        userMap = new HashMap<>();
+        postings = new HashMap<>();
+    }
+
+    // POST
+    // public Posting addPosting(Profile profile, Posting posting) {
+    //     posting.setPostingId(nextPostingId++);
+    //     posting.setDate(LocalDateTime.now());
+    //     posting.setUserName(profile.getUserName());
+    //     postings.put(posting.getPostingId(), posting);
+    //     profile.addPosting(posting);
+    //     return posting;
+    // }
+
+    // Remove the duplicate method addProfile(Profile)
+    // public Profile addProfile(Profile profile) {
+    //     Profile newProfile = null;
+    //     if (userMap.get(profile.getUserName()) == null) {
+    //         newProfile = profile;
+    //         newProfile.setProfileId(nextProfileId++);
+    //         profiles.put(newProfile.getProfileId(), newProfile);
+    //         userMap.put(newProfile.getUserName(), newProfile);
+    //     }
+    //     return newProfile;
+    // }
+
+    // GET ALL
+
+    public List<Profile> getProfiles() {
+        return new ArrayList<>(profiles.values());
+    }
+
+    public List<Posting> getPostings() {
+        return new ArrayList<>(postings.values());
+    }
+
+    // GET BY ???
+
+    public Profile getProfileById(Integer id) {
+        Profile profile = profiles.get(id);
+        return profile;
+    }
+    
+
+    public List<Profile> getProfilesByUserName(String userName) {
+        Profile profile = userMap.get(userName);
+        return new ArrayList<>(Arrays.asList(profile));
+    }
+
+    public ResponseEntity<Posting> addPosting(Profile profile, Posting posting) {
+        posting.setPostingId(nextPostingId++);
+        posting.setDate(LocalDateTime.now());
+        posting.setUserName(profile.getUserName());
+        postings.put(posting.getPostingId(), posting);
+        profile.addPosting(posting);
+
+        return new ResponseEntity<>(posting, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Profile> addProfile(Profile profile) {
+        Profile newProfile = null;
+        if (userMap.get(profile.getUserName()) == null) {
+            newProfile = profile;
+            newProfile.setProfileId(nextProfileId++);
+            profiles.put(newProfile.getProfileId(), newProfile);
+            userMap.put(newProfile.getUserName(), newProfile);
+        }
+        return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
+    }
+
+    public boolean deletePosting(Profile profile, Integer postingid)
+    {
+        List<Posting> postings = profile.getPostings();
+        for (Posting posting : postings) {
+            if(posting.getPostingId().equals(postingid))
+            {
+                System.out.println("here3");
+                postings.remove(posting);
+                return  true;
+            }
+        } 
+        return false;
+    }
+    public boolean deleteAllPostings(Profile profile)
+    {
+        List<Posting> postings = profile.getPostings();
+        Boolean deleted = false;
+
+        if(!postings.isEmpty())
+        {
+            postings.clear();
+            deleted = true;
+        }
+
+        return deleted;
+    }
+    public Boolean deleteProfileById(Profile profile) {
+
+        if (profiles.containsValue(profile)) {
+            userMap.remove(profile.getUserName());
+            profiles.remove(profile.getProfileId());
+
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean updatePosting(Profile profile, Posting updatePosting, Integer postingid) {
+
+    
+        List<Posting> postingList = profile.getPostings();
+        for (Posting posting : postingList) {
+            if(posting.getPostingId().equals(postingid))
+            { 
+                posting.setPostingText(updatePosting.getPostingText());
+                postings.put(postingid, posting);
+                return  true;
+            }
+        } 
+         
+
+
+        
+        return false;
+    } 
+
+}
